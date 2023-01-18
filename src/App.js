c5468c0b6d5f7726cbc0f6dd35768c7f0a27d9f6
jacobplumblambda <dhagehassan@gmail.com>
2023-01-18 10:10:45 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import { TodoList } from './components/TodoList';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Logout</Button>
+        </Toolbar>
+      </AppBar>
+      <TodoList />
+    </Box>
   );
 }
 
---
+++ src/components/TodoList.js
@@ -0,0 +1,5 @@
+export const TodoList = (props) => {
+    return (
+        <h1>Todo List</h1>
+    )
+}
